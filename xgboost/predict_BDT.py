4e6cdc84c33f5425405771e99433674b9c21caa9
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-10 19:42:28 +0100

    Update predict_BDT.py

--- xgboost/predict_BDT.py
+++ xgboost/predict_BDT.py
@@ -61,12 +61,13 @@ def predict_BDT_model(files, name, config, date, categories=None):
         for i in range(number_of_splits):
             fold_name = f"fold_{i}_"
             model.data[fold_name] = model.data[fold_name+category_list[0]+'_']
-            #model.data = model.data.drop(fold_name+category_list[0]+'_', axis=1)
+            model.data = model.data.drop(fold_name+category_list[0]+'_', axis=1, errors='ignore')
             for j in range(1, N_cat):
                 model.data[fold_name] = model.data[fold_name].combine_first(model.data[fold_name+category_list[j]+'_'])
                 #model.data = model.data.drop(fold_name+category_list[i]+'_', axis=1)
 
         print(model.data)
+        print(model.data['fold_0_Cat_A_'])
 
         
 
