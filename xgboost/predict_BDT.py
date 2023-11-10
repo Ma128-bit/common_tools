acf1c4d170d9f4b402ee5374facf48a1f87f2bdc
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-10 17:30:26 +0100

    Update predict_BDT.py

--- xgboost/predict_BDT.py
+++ xgboost/predict_BDT.py
@@ -4,7 +4,7 @@ from ModelHandler import *
 class MuonMVA(ModelHandler):
     pass
 
-def predict_BDT_model(files, name, config, date, category=None):
+def predict_BDT_model(files, name, config, date, categories=None):
     model = MuonMVA(config)
     model.load_datasets(files, config)
 
@@ -18,12 +18,23 @@ def predict_BDT_model(files, name, config, date, category=None):
     for key in selections_keys:
         model.apply_selection(config, key)
 
-    if category is None:
+    if categories is None:
         models = model.load_models(name, date)
         model.predict_models(models, name)
         model.mk_bdt_score(models)
         print(model.data.columns.tolist())
         print(model.data)
+
+    if categories is not None:
+        models = model.load_models(name, date)
+        category_list = categories.split(' ')
+        models_per_cat = {k: {} for k in category_list}
+        for key, value in models.items():
+            for category in category_list:
+                if category in key:
+                    models_per_cat[category][key] = value
+        
+
 
 
 
@@ -31,12 +42,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the BDT training")
 
     parser.add_argument("--config", type=str, help="Path to the JSON configuration file")
-    parser.add_argument("--category", type=str, help="Name of the category")
+    parser.add_argument("--categories", type=str, help="List of categories separated by ' ' ")
 
     args = parser.parse_args()
     
     config = args.config
-    category = args.category
+    categories = args.categories
     
     with open(config, 'r') as file:
         json_file = json.load(file)
@@ -47,7 +58,7 @@ if __name__ == "__main__":
         
     files_Run2022 = [data_path + i for i in files]
     
-    predict_BDT_model(files_Run2022, name, config, date, category)
+    predict_BDT_model(files_Run2022, name, config, date, categories)
     #Fa il train sui number_of_splits fold ma poi manca la parte dove vengono aggrgati (?)
     #Quindi bisogna creare un nuovo file .root con il nuovo branch
     #Implementare la divisione in categorie per tau3mu
