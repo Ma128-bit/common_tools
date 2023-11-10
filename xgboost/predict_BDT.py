25092a8b3864d7979ecd0896e700453b4bd61349
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-10 22:43:05 +0100

    Update predict_BDT.py

--- xgboost/predict_BDT.py
+++ xgboost/predict_BDT.py
@@ -42,7 +42,7 @@ def predict_BDT_model(files, name, config, date, categories=None):
             model.data = model.data[model.data[category_lable] == i]
             df_index.append(model.data.index.to_series())
             features_imp = model.predict_models(models_per_cat[category_list[i]], name)
-            plot_feature_importance(features_imp, category_list[i], name, date)
+            model.plot_feature_importance(features_imp, category_list[i], name, date)
             model.mk_bdt_score(models_per_cat[category_list[i]])
             out_df.append(model.data)
             print("End ", category_list[i])
