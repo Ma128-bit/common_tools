6e684adc6a36537f618154149ec8e670680a873d
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-10 15:59:33 +0100

    Update ModelHandler.py

--- xgboost/ModelHandler.py
+++ xgboost/ModelHandler.py
@@ -224,7 +224,7 @@ class ModelHandler:
         lable_name = model_name+"-Event"
         directory = self.output_path + "/" + date
         print(directory)
-        file_list = os.listdir(self.output_path)
+        file_list = os.listdir(directory)
         print(file_list)
         model_names = [file for file in file_list if file.startswith(lable_name) and file.endswith(".pkl")]
         print(model_names)
