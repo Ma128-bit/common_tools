7ee388a1837f0f730c79827ba1408e5f360a1c78
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-10 21:36:58 +0100

    Update predict_BDT.py

--- xgboost/predict_BDT.py
+++ xgboost/predict_BDT.py
@@ -47,7 +47,7 @@ def predict_BDT_model(files, name, config, date, categories=None):
             print(model.data)
             model.data = data_copy.copy()
         
-        total_index  = pd.Int64Index([])
+        total_index  = pd.Index([])
         for i in range(len(df_index)):
             total_index = pd.concat(total_index, df_index[i])
             
