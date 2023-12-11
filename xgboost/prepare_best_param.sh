b9099bc62a90650656300cb6d694d38844fb6fc8
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-11 16:42:40 +0100

    Update prepare_best_param.sh

--- xgboost/prepare_best_param.sh
+++ xgboost/prepare_best_param.sh
@@ -40,6 +40,7 @@ cp ./templates/submit.condor ./$real_out
 sed -i "s#launch_training.sh#launch_best_param.sh#g" ./${real_out}/submit.condor
 sed -i "s#PATH#${real_out}#g" ./${real_out}/submit.condor
 number_of_submit=10
+echo "request_cpus​=​16" >> "./${real_out}/submit.condor"
 echo "queue ${number_of_submit}" >> "./${real_out}/submit.condor"
 chmod a+x ./${real_out}/submit.condor
 
