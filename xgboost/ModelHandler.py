c1d01cdd5d24b5e02ce35275e974ac6dd107bf64
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-11 12:00:13 +0100

    Update ModelHandler.py

--- xgboost/ModelHandler.py
+++ xgboost/ModelHandler.py
@@ -1,14 +1,4 @@
 import sys, os, subprocess, json
-import warnings, traceback
-#warnings.filterwarnings("ignore", category=UserWarning, module="numpy")
-
-def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
-    log = file if hasattr(file, 'write') else sys.stderr
-    traceback.print_stack(file=log)
-    log.write(warnings.formatwarning(message, category, filename, lineno, line))
-
-warnings.showwarning = warn_with_traceback
-warnings.filterwarnings("always", category=UserWarning)
 from datetime import datetime
 import pandas as pd
 import uproot
