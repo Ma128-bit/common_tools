42010733be4cb5c3e36c533e3f03d96d38237ff0
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-12 21:08:21 +0100

    Update find_best_param.py

--- xgboost/find_best_param.py
+++ xgboost/find_best_param.py
@@ -76,7 +76,7 @@ def best_par(files_Run2022, name, config, date, random=0, condor=False):
         xgbR, param_distributions=param_dist, n_iter=10, verbose=2, scoring='roc_auc', cv=cv, random_state=(42 + random * 15), n_jobs=N_jobs
     )
     
-    random_search.fit(model.x_train, model.y_train, verbose=False, sample_weight=model.train_weights, eval_set=[(model.x_train, model.y_train)], stratify=model.y_train)
+    random_search.fit(model.x_train, model.y_train, verbose=False, sample_weight=model.train_weights, eval_set=[(model.x_train, model.y_train)])
     """    
     random_search = RandomizedSearchCV(
         xgbR, param_distributions=param_dist, n_iter=10, verbose=2, scoring='roc_auc', cv=2, random_state=(42+random*15), n_jobs=N_jobs
