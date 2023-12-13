206dc3669ff14ccc636912f3b640bb5d5ea04bf0
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-12-13 10:21:43 +0100

    Update find_best_param.py

--- xgboost/find_best_param.py
+++ xgboost/find_best_param.py
@@ -73,7 +73,7 @@ def best_par(files_Run2022, name, config, date, random=0, condor=False):
     cv = StratifiedKFold(n_splits=4, shuffle=True, random_state=(42 + random * 15))
     
     random_search = RandomizedSearchCV(
-        xgbR, param_distributions=param_dist, n_iter=60, verbose=2, scoring='roc_auc', cv=cv, random_state=(42 + random * 15), n_jobs=N_jobs
+        xgbR, param_distributions=param_dist, n_iter=40, verbose=2, scoring='roc_auc', cv=cv, random_state=(42 + random * 15), n_jobs=N_jobs
     )
     
     random_search.fit(model.x_train, model.y_train, verbose=False, sample_weight=model.train_weights, eval_set=[(model.x_train, model.y_train)])
