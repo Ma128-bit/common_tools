fcbedd1e35d92172e5aeba0b93844d58d73bba35
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-24 17:46:15 +0100

    Update best_AUC_score.py

--- xgboost/best_AUC_score.py
+++ xgboost/best_AUC_score.py
@@ -114,7 +114,7 @@ def histo_AMS(names, category, type="all", weights = True):
         vector = best_AUC_in_category(name, category, weights)
         auc_test.append(vector[1])
         auc_train.append(vector[0])
-    colori = ['red' if valore > (max(auc_test)-min(auc_test))%10 else 'blue' for valore in auc_test]
+    colori = ['red' if valore > max(auc_test)-(max(auc_test)-min(auc_test))/10 else 'blue' for valore in auc_test]
     plt.bar(names, auc_test, color=colori)
     plt.xticks(rotation='vertical')
     #ax.set_xlabel("File")
