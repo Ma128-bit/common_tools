e790462cb145a730388df47c25f189007e748665
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-24 11:49:25 +0100

    Update best_AUC_score.py

--- xgboost/best_AUC_score.py
+++ xgboost/best_AUC_score.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-global n_splits = 5
-global out_path = "results/BDT/"
+n_splits = 5
+out_path = "results/BDT/"
+
 def best_AUC_in_category(name, category, ax, sel = "max", weights = True):
     if weights = True:
         w = "-w"
