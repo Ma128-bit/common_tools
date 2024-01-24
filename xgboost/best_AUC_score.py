db2a176135ccd0186c8aaa86257513682a19a205
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-24 17:20:19 +0100

    Update best_AUC_score.py

--- xgboost/best_AUC_score.py
+++ xgboost/best_AUC_score.py
@@ -5,7 +5,7 @@ n_splits = 5
 out_path = "results/ROCurvs/"
 in_path = "results/BDT/"
 
-def best_AUC_in_category(name, category, ax, type="all", sel = "max", weights = True):
+def best_ROC_in_category(name, category, ax, type="all", sel = "max", weights = True):
     if category=="A":
         trs_test = 0.915
     elif category=="B":
@@ -60,13 +60,37 @@ def best_AUC_in_category(name, category, ax, type="all", sel = "max", weights = 
             ax.plot(fpr_train, tpr_train, label="train "+name+" AUC = {:.6f}".format(auc_train))
             ax.plot(fpr_test, tpr_test, label="test "+name+" AUC = {:.6f}".format(auc_test))
 
+def best_AUC_in_category(name, category, weights = True):
+    if weights == True:
+        w = "-w"
+        w2 = "_w"
+        w3 = "weighted_"
+    else:
+        w=""
+        w2=""
+        w3=""
+    file = f"-Category_Cat_{category}-validation-roc"+w+".npz"
+    auc_test_mean = 0
+    auc_train_mean = 0
+    for i in range(n_splits):
+        file_name = in_path + name +"/" + f"Run2022-Event{i}" + file
+        loaded_data = np.load(file_name)
+        auc_test_temp = loaded_data[w3+"auc_test"]
+        auc_train_temp = loaded_data[w3+"auc_train"]
+        auc_test_mean = auc_test_mean + auc_test_temp
+        auc_train_mean = auc_train_mean + auc_train_temp
+        del loaded_data
+        
+    auc_test_mean = auc_test_mean/n_splits
+    auc_train_mean = auc_train_mean/n_splits
 
+    return [auc_train_mean, auc_test_mean]
 
 def draw_category(names, category, type="all", sel = "max", weights = True):
     fig, ax = plt.subplots()
     plt.grid(which='both', axis='both')
     for name in names:
-        best_AUC_in_category(name, category, ax, type, sel, weights)
+        best_ROC_in_category(name, category, ax, type, sel, weights)
     ax.set_xlabel("bkg eff")
     ax.set_ylabel("sig eff")
     ax.set_xlim([0.05, 1])
@@ -77,19 +101,37 @@ def draw_category(names, category, type="all", sel = "max", weights = True):
     #fig.set_tight_layout(True)
     fig.savefig(out_path +"Category_"+category+"_type_"+type+"_sel_"+sel+"-roc_"+ names[0].split('-')[0]+".pdf")
 
+def histo_AMS(names, category, type="all", weights = True):
+    auc_test = []
+    auc_train = []
+    for name in names:
+        vector = best_AUC_in_category(name, category, weights)
+        auc_test.append(vector[1])
+        auc_train.append(vector[0])
+        
+    plt.bar(names, auc_test)
+    ax.set_xlabel("File")
+    ax.set_ylabel("AUC")
+    ax.set_ylim([0.8, 1])
+    #ax.set_yscale("log")
+    ax.set_title("ROC curves")
+    #ax.legend()
+    fig.savefig(out_path +"Category_"+category+"_type_"+type+"_sel_"+sel+"-auc_"+ names[0].split('-')[0]+".pdf")
+
 
 if __name__ == "__main__":
     #files = ["20240124-104401", "20240124-122044", "20240124-132322", "20240124-134302", "20240124-134256", "20240124-134305", "20240124-134308", "20240124-134311", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ]
     
     list = os.listdir(in_path)
     files = [i for i in list if os.path.isdir(os.path.join(in_path, i)) and "20240124" in i]
-
+    histo_AMS(files, "A", type="all")
+    """
     draw_category(files, "A", type="test", sel ="min")
     draw_category(files, "B", type="test", sel ="min")
     draw_category(files, "C", type="test", sel ="min")
     draw_category(files, "A", type="train", sel ="min")
     draw_category(files, "B", type="train", sel ="min")
     draw_category(files, "C", type="train", sel ="min")
-
+    """
 
 
