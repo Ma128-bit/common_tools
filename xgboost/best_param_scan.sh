d6dfbe2faedbc26443fd85c0fb3772ada84d32ea
Marco <127106076+Ma128-bit@users.noreply.github.com>
2024-01-24 13:13:30 +0100

    Update best_param_scan.sh

--- xgboost/best_param_scan.sh
+++ xgboost/best_param_scan.sh
@@ -12,7 +12,7 @@ declare -a gamma=(0.01 0.1 0.5 1)
 declare -a reg_alpha=(0.01 0.1 0.5 1)
 declare -a reg_lambda=(0.01 0.1 0.5 1)
 
-for i in {0..7}; do
+for i in {0..0}; do
   random_I_max_depth=$((RANDOM % 4))
   random_max_depth=${max_depth[$random_I_max_depth]}
   
@@ -40,7 +40,18 @@ for i in {0..7}; do
   random_I_reg_lambda=$((RANDOM % 4))
   random_reg_lambda=${reg_lambda[$random_I_reg_lambda]}
 
+  cp ./config/config_tau3mu_template.json ./config_tau3mu.json
+  sed -i "s#max_depth_val#${random_max_depth}#g" ./config_tau3mu.json
+  sed -i "s#learning_rate_val#${random_learning_rate}#g" ./config_tau3mu.json
+  sed -i "s#n_estimators_val#${random_n_estimators}#g" ./config_tau3mu.json
+  sed -i "s#subsample_val#${random_subsample}#g" ./config_tau3mu.json
+  sed -i "s#colsample_bytree_val#${random_colsample_bytree}#g" ./config_tau3mu.json
+  sed -i "s#min_child_weight_val#${random_min_child_weight}#g" ./config_tau3mu.json
+  sed -i "s#gamma_val#${random_gamma}#g" ./config_tau3mu.json
+  sed -i "s#reg_alpha_val#${random_reg_alpha}#g" ./config_tau3mu.json
+  sed -i "s#reg_lambda_val#${random_reg_lambda}#g" ./config_tau3mu.json
 
+  
   echo "Elemento casuale: ${random_max_depth}, ${random_learning_rate}, ${random_n_estimators}, ${random_subsample}, ${random_colsample_bytree}, ${random_min_child_weight}, ${random_gamma}, ${random_reg_alpha}, ${random_reg_lambda}"
   done
   
